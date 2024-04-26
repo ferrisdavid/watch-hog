08772c1ba9b140ffb81884ff623b8de36171d988
ferrisdavid <davidferris195@gmail.com>
2024-04-26 17:18:21 -0400

    fixed middleware route protection

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,15 @@
+// Next.
+import { NextResponse } from "next/server"
+
+// Auth.
+import { auth } from "./auth"
+ 
+export default auth((req) => {
+  if (!req.auth) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+})
 
 export const config = {
-    matcher: ["/feed", "/social", "/profile"],
+    matcher: ["/explore", "/feed", "/social", "/profile"],
 }
