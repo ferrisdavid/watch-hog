0b36e536a59e12cb433293ff43230809a96d28da
ferrisdavid <davidferris195@gmail.com>
2024-04-26 17:19:33 -0400

    Adjusted app padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+// Auth.
+import { auth } from '@/auth';
+
 // DB.
 import { prisma } from '@/lib/prisma';
 
@@ -6,14 +9,9 @@ import Image from 'next/image'
 
 export default async function Home() {
 
-  const users = await prisma.user.findFirst({ where: {
-    email: "test@test.com"
-  }});
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        {users?.name}
       </div>
     </main>
   )
