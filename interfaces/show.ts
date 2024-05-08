792daf70e6231aba8dee7ccf5f845a2c508bbe1d
ferrisdavid <davidferris195@gmail.com>
2024-05-08 00:09:21 -0400

    Added interface for TV Show data type returned by film api

---
+++ interfaces/show.ts
@@ -0,0 +1,17 @@
+export interface ITVShow {
+  backdrop_path: string;
+  id: number;
+  original_name: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  adult: false;
+  name: string;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  first_air_date: string;
+  vote_average: number;
+  vote_count: number;
+  origin_country: string[];
+}
