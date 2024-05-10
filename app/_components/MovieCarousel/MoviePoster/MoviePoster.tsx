8ef789590b9513adba5178aa4d042f77eb8f4049
ferrisdavid <davidferris195@gmail.com>
2024-05-10 17:04:45 -0400

    show/movie poster component redesign in carousels + fix for unequal heights

--- app/_components/MovieCarousel/MoviePoster/MoviePoster.tsx
+++ app/_components/MovieCarousel/MoviePoster/MoviePoster.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 
 // Icons.
-import { FiStar } from "react-icons/fi";
+import { FiHeart, FiStar } from "react-icons/fi";
 
 // Components.
 import { Title } from "@mantine/core";
@@ -19,9 +19,25 @@ import styles from "./MoviePoster.module.scss";
 // Props.
 interface Props {
   movie: IMovie;
+  imgType?: "poster" | "backdrop";
 }
 
-export default function MoviePoster({ movie }: Props) {
+export default function MoviePoster({ movie, imgType = "poster" }: Props) {
+  const imgConfig: {
+    [key: string]: { path: string; size: "w500" | "original"; style: string };
+  } = {
+    poster: {
+      path: movie.poster_path,
+      size: "w500",
+      style: "aspect-[2/3]",
+    },
+    backdrop: {
+      path: movie.backdrop_path,
+      size: "original",
+      style: "aspect-video min-h-[420px]",
+    },
+  };
+
   return (
     <div className="relative transition-all hover:cursor-pointer hover:scale-x-105 z-1">
       <div
@@ -29,24 +45,63 @@ export default function MoviePoster({ movie }: Props) {
       />
 
       <Image
-        className="rounded h-[100%] w-[100%]"
-        src={formatPosterUrl(movie.poster_path) || ""}
+        className={`rounded border border-card-border/20 h-[100%] w-[100%] ${imgConfig[imgType].style}`}
+        src={
+          formatPosterUrl(imgConfig[imgType].path, imgConfig[imgType].size) ||
+          ""
+        }
         alt={`${movie.title} poster`}
         width={200}
         height={250}
       />
 
-      <div className="flex flex-col justify-between gap-2 text-text-secondary absolute bottom-0 p-2 rounded-b w-[100%]">
-        <div>
-          <Title order={3} className="!text-[18px] !line-clamp-2	">
-            {movie.title}
-          </Title>
-        </div>
-
-        <h4 className="flex items-center gap-1 text-bg-light-purple text-sm font-medium">
+      {/* Poster Top Info Section */}
+      <div className="absolute top-3 left-3">
+        <h4
+          className={`bg-card-border/60 flex items-center gap-1 text-bg-light-purple text-sm font-medium rounded h-6 px-2`}
+        >
           <FiStar size={16} /> {movie.vote_average.toPrecision(2)}
         </h4>
       </div>
+
+      <div className="absolute top-3 right-3">
+        <h4
+          className={`bg-card-border/60 flex items-center justify-center gap-1 text-bg-light-purple text-sm font-medium rounded w-8 h-8`}
+        >
+          <FiHeart size={18} />
+        </h4>
+      </div>
+
+      {/* Poster Bottom Info Section */}
+      <div className="flex flex-col justify-between gap-2 text-text-secondary absolute bottom-0 p-2 rounded-b w-[100%]">
+        <div className="flex flex-col gap-1">
+          {imgType == "poster" && (
+            <>
+              <Title order={3} className="!text-[18px] !line-clamp-2">
+                {movie.title}
+              </Title>
+              <h4 className="text-bg-light-purple text-sm font-medium">
+                {movie.release_date.slice(0, 4)}
+              </h4>
+            </>
+          )}
+
+          {imgType == "backdrop" && (
+            <>
+              <Title order={2} className="!line-clamp-2">
+                {movie.title}
+              </Title>
+              <Title order={5} className="!text-bg-light-purple">
+                {movie.media_type.toUpperCase()} • {movie.release_date} • {movie.original_language.toUpperCase()}
+              </Title>
+              <Title order={6} className="!line-clamp-2">
+                {movie.overview}
+              </Title>
+            </>
+          )}
+        </div>
+      </div>
+
     </div>
   );
 }
--- app/_components/TVCarousel/ShowPoster/ShowPoster.tsx
+++ app/_components/TVCarousel/ShowPoster/ShowPoster.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 
 // Icons.
-import { FiStar } from "react-icons/fi";
+import { FiHeart, FiStar } from "react-icons/fi";
 
 // Components.
 import { Title } from "@mantine/core";
@@ -27,10 +27,18 @@ interface Props {
 
 export default function ShowPoster({ show, imgType = "poster" }: Props) {
   const imgConfig: {
-    [key: string]: { path: string; size: "w500" | "original" };
+    [key: string]: { path: string; size: "w500" | "original"; style: string };
   } = {
-    poster: { path: show.poster_path, size: "w500" },
-    backdrop: { path: show.backdrop_path, size: "original" },
+    poster: {
+      path: show.poster_path,
+      size: "w500",
+      style: "aspect-[2/3]",
+    },
+    backdrop: {
+      path: show.backdrop_path,
+      size: "original",
+      style: "aspect-video min-h-[420px]",
+    },
   };
 
   return (
@@ -40,9 +48,7 @@ export default function ShowPoster({ show, imgType = "poster" }: Props) {
       />
 
       <Image
-        className={`rounded h-[100%] w-[100%] min-h-[235px] ${
-          imgType == "backdrop" && "aspect-video min-h-[420px]"
-        }`}
+        className={`rounded border border-card-border/20 h-[100%] w-[100%] ${imgConfig[imgType].style}`}
         src={
           formatPosterUrl(imgConfig[imgType].path, imgConfig[imgType].size) ||
           ""
@@ -52,28 +58,52 @@ export default function ShowPoster({ show, imgType = "poster" }: Props) {
         height={1000}
       />
 
-      <div className="flex flex-col justify-between gap-2 text-text-secondary absolute bottom-0 p-2 rounded-b w-[100%]">
-        <div className="flex flex-col gap-5">
-          <Title order={3} className="!text-[18px] !line-clamp-2">
-            {show.name}
-          </Title>
-
-          {imgType == "backdrop" && (
-            <>
-              <Title order={5} className="!text-bg-light-purple">
-                {show.media_type.toUpperCase()} - {show.first_air_date} -{" "}
-                {show.original_language.toUpperCase()}
-              </Title>
-              <Title order={6} className="!line-clamp-2">
-                {show.overview}
-              </Title>
-            </>
-          )}
-        </div>
-
-        <h4 className="flex items-center gap-1 text-bg-light-purple text-sm font-medium">
+      {/* Poster Top Info Section */}
+      <div className="absolute top-3 left-3">
+        <h4
+          className={`bg-card-border/60 flex items-center gap-1 text-bg-light-purple text-sm font-medium rounded h-6 px-2`}
+        >
           <FiStar size={16} /> {show.vote_average.toPrecision(2)}
         </h4>
+      </div>
+
+      <div className="absolute top-3 right-3">
+        <h4
+          className={`bg-card-border/60 flex items-center justify-center gap-1 text-bg-light-purple text-sm font-medium rounded w-8 h-8`}
+        >
+          <FiHeart size={18} />
+        </h4>
+      </div>
+
+      {/* Poster Bottom Info Section */}
+      <div className={`flex flex-col justify-between gap-2 text-text-secondary absolute bottom-0 rounded-b w-[100%] ${imgType == "poster" ? 'p-2' : 'p-10'}`}>
+        {imgType == "poster" && (
+          <div className="flex flex-col gap-1">
+            <Title order={3} className="!text-[18px] !line-clamp-2">
+              {show.name}
+            </Title>
+            <h4 className="text-bg-light-purple text-sm font-medium">
+              {show.first_air_date.slice(0, 4)}
+            </h4>
+          </div>
+        )}
+
+        {imgType == "backdrop" && (
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <Title order={2} className="!line-clamp-2 gap">
+                {show.name}
+              </Title>
+              <Title order={5} className="!text-bg-light-purple">
+                {show.media_type.toUpperCase()} • {show.first_air_date} •{" "}
+                {show.original_language.toUpperCase()}
+              </Title>
+            </div>
+            <Title order={6} className="!line-clamp-2">
+              {show.overview}
+            </Title>
+          </div>
+        )}
       </div>
     </div>
   );
