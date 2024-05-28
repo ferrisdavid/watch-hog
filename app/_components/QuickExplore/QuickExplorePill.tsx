c907bdc9ed1cb470d6c7d85f077c99961f821075
ferrisdavid <davidferris195@gmail.com>
2024-05-27 20:44:59 -0400

    Changed quick explore pill to display provider logos

--- app/_components/QuickExplore/QuickExplorePill.tsx
+++ app/_components/QuickExplore/QuickExplorePill.tsx
@@ -1,15 +1,26 @@
-// Interfaces.
-import { IProvider } from "@/interfaces/providers";
+// Components
+import Image from "next/image";
+
+// Utils.
+import { formatPosterUrl } from "../Trending/utils/movieFormatter";
 
 // Props.
 interface Props {
-    provider: IProvider;
+  provider: string;
+  providerLogo: string;
 }
 
-export default function QuickExplorePill({ provider }: Props) {
-    return (
-        <div className="flex items-center justify-center bg-text-secondary grow rounded-md border border-card-border/30 shadow-sm min-h-[32px]">
-            {provider}
-        </div>
-    )
+export default function QuickExplorePill({ provider, providerLogo }: Props) {
+  return (
+    <div className="flex min-w-[85px] min-h-[85px] items-center justify-center grow transition-all hover:cursor-pointer hover:z-10 hover:scale-105">
+      <Image
+        className="w-[100%] rounded-md border border-card-border/20 shadow-sm"
+        src={formatPosterUrl(providerLogo, "original")}
+        alt={`${provider}-logo`}
+        width={85}
+        height={85}
+        quality={100}
+      />
+    </div>
+  );
 }
