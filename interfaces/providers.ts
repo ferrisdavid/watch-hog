13655bffb0609ed121bc131164a1cc14d6ddcd48
ferrisdavid <davidferris195@gmail.com>
2024-05-27 20:28:46 -0400

    Added type interface for provider response data from API

--- interfaces/providers.ts
+++ interfaces/providers.ts
@@ -1,1 +1,12 @@
 export type IProvider = "netflix" | "prime" | "disney";
+
+
+export interface IProvidersResponse {
+    display_priorities: {
+        [country_code: string]: number
+    };
+    display_priority: number,
+    logo_path: string,
+    provider_name: string,
+    provider_id: number
+}
