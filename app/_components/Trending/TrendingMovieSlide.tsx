a73359a8e31e3c21d8bdd7e31a8f09188612b42e
ferrisdavid <davidferris195@gmail.com>
2024-05-27 20:44:05 -0400

    changed trending movie slide to new hero style

--- app/_components/Trending/TrendingMovieSlide.tsx
+++ app/_components/Trending/TrendingMovieSlide.tsx
@@ -24,44 +24,52 @@ interface Props {
 export default function TrendingMovieSlide({ movie, genres, rank }: Props) {
   return (
     <>
-      <div className="flex grow flex-col gap-5 p-12 h-[100%]">
-        <h2 className="text-dark-purple text-3xl font-bold">
-          #{rank}. {movie.title}
-        </h2>
-        <h4 className="flex items-center gap-1 text-card-border text-lg font-medium">
-         <FiStar size={16}/> {movie.vote_average.toPrecision(2)} - {movie.media_type} - {movie.release_date} - {formatMovieGenres(movie.genre_ids, genres)}
-        </h4>
-        <p className="text-dark-purple mt-6 font-medium">
-          {movie.overview}
-        </p>
-        <div className="flex gap-2 justify-between items-center">
-          <div id="provider-logos"></div>
-          <div className="flex gap-2">
-            <Button className="flex items-center justify-center !bg-text-secondary">
-              <FiHelpCircle className="text-bg-purple" size={24} />
-            </Button>
-            <Button className="flex items-center justify-center !bg-dark-purple">
-              <FiHeart className="text-text-secondary" size={24} />
-            </Button>
-          </div>
-        </div>
-      </div>
-
-      <div className="relative p-12 min-w-[50%] flex grow self-center items-center justify-center">
+      <div className="relative w-[100%] flex grow self-center items-center justify-center">
         <Image
-          src={formatPosterUrl(movie.poster_path) || ""}
-          alt="backdrop"
-          className="absolute p-5 bg-center z-[-1] blur-3xl opacity-50"
-          width={900}
-          height={300}
+          src={formatPosterUrl(movie.backdrop_path, "original") || ""}
+          alt={`${movie.title} poster`}
+          className="w-[100%] max-h-[750px] aspect-square"
+          width={"1920"}
+          height={"1080"}
+          quality={100}
         />
 
-        <Image
-          src={formatPosterUrl(movie.poster_path) || ""}
-          alt={`${movie.title} poster`}
-          className="rounded aspect-square"
-          width={"400"}
-          height={"200"}
+        <div
+          id="meta-overlay"
+          className="absolute left-0 w-[80%] min-h-[100%] bg-gradient-to-r from-bg-primary/60 from-25%"
+        />
+
+        <div className="absolute left-0 bottom-[15%] w-[45%] flex flex-col gap-3 p-14">
+          <h1 className="text-text-secondary text-6xl font-bold line-clamp-2">
+            {movie.title}
+          </h1>
+          <h3 className="text-text-secondary text-xl font-medium line-clamp-2">
+            Trending <span className="text-bg-light-purple">#{rank}</span> Today
+          </h3>
+          <h4 className="flex items-center gap-1 text-text-secondary text-lg font-medium line-clamp-2 capitalize">
+            <FiStar size={16} className="text-bg-light-purple" />{" "}
+            {movie.vote_average.toPrecision(2)} • {movie.media_type} •{" "}
+            {movie.release_date} • {formatMovieGenres(movie.genre_ids, genres)}
+          </h4>
+          <p className="text-text-secondary mt-6 font-medium line-clamp-4">
+            {movie.overview}
+          </p>
+          <div className="flex gap-2 justify-between items-center">
+            <div id="provider-logos"></div>
+            <div className="flex gap-2">
+              <Button className="flex items-center justify-center !bg-text-secondary">
+                <FiHelpCircle className="text-bg-purple" size={24} />
+              </Button>
+              <Button className="flex items-center justify-center !bg-dark-purple">
+                <FiHeart className="text-text-secondary" size={24} />
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <div
+          id="bottom-blend-overlay"
+          className="absolute bottom-0 left-0 w-[100%] min-h-[150px] bg-gradient-to-t from-bg-primary from-15%"
         />
       </div>
     </>
