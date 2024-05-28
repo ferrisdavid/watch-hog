a52ab70a41a08a28d62022365051dfc19f2f0e5a
ferrisdavid <davidferris195@gmail.com>
2024-05-27 20:45:47 -0400

    Added data fetch function watch providers (netflix, disney, etc)

--- app/utils/providers/get-available-providers.ts
+++ app/utils/providers/get-available-providers.ts
@@ -1,6 +1,15 @@
 // Interfaces.
+import { IProvidersResponse } from "@/interfaces/providers";
 
-export async function getAvailableProviders(): Promise<{}> {
+export async function getAvailableProviders(): Promise<{results: IProvidersResponse[]}> {
     // Watch Providers URL
-    const url: string = process.env.API_URL + "";
+    const url: string = process.env.API_URL + "/watch/providers/movie?language=en-US&watch_region=CA";
+
+    const response = await fetch(url, { headers: { accept: 'application/json', Authorization: `Bearer ${process.env.ACCESS_TOKEN}` } });
+
+    if (!response.ok) {
+        throw new Error("Failed to Fetch Trending Movies");
+    }
+
+    return response.json();
 }
